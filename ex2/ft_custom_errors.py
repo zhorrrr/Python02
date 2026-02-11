5804a569f7850ed05de1d9053282d4ecb541d114
zhorrrr <fzhraeelm@gmail.com>
2026-02-11 12:11:06 +0100

    h

--- ex2/ft_custom_errors.py
+++ ex2/ft_custom_errors.py
@@ -8,39 +8,43 @@ class WaterError(GardenError):
     pass
 
 def check_plant(plant: str) -> None:
-    if plant == "":
-        raise PlantError("Plant name cannot be empty!")
+    if plant == "wilting":
+        raise PlantError("The tomato plant is wilting!")
     print(f"Plant '{plant}' is healthy!")
 
-def check_water(water_amount: int) -> None:
-    if water_amount < 0:
-        raise WaterError("Water amount cannot be negative!")
+def check_water(water_amount : int) -> None:
+    if water_amount < 10:
+        raise WaterError("Not enough water in the tank!")
     print(f"Water amount {water_amount}ml is sufficient!")
 
 def test_errors() -> None:
     print("Testing PlantError...")
     try:
-        check_plant("")
-    except PlantError:
-        print("Caught PlantError: Plant name cannot be empty!\n")
+        check_plant("wilting")
+    except PlantError as error:
+        print(f"Caught PlantError: {error}")
     
-
-    print("Testing WaterError...")
+    print("\nTesting WaterError...")
     try:
-        check_water(-5)
-    except WaterError:
-        print("Caught WaterError: Water amount cannot be negative!\n")
+        check_water(5)
+    except WaterError as error:
+        print(f"Caught WaterError: {error}")
     
     print("\nTesting catching all garden errors...")
     try:
-        check_plant("")
-    except GardenError:
-        print("Caught PlantError via GardenError parent!\n")
+        check_plant("wilting")
+    except GardenError as error:
+        print(f"Caught a garden error: {error}")
+    
+    try:
+        check_water(5)
+    except GardenError as error:
+        print(f"Caught a garden error: {error}")
 
-def ft_custom_errors() -> None:
+def ft_custom_errors():
     print("=== Custom Garden Errors Demo ===\n")
     test_errors()
-    print("\nAll tests completed - program didn't crash!")
+    print("\nAll custom error types work correctly!")
 
 if __name__ == "__main__":
     ft_custom_errors()
